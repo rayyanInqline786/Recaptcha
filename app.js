5f6ff756d91db1666fb0b0c8f9da4e2e5bfbe7c7
rayyanInqline786 <rayyan@inqline.com>
2020-10-12 12:21:25 -0400

    reacptcha pushed

---
+++ app.js
@@ -0,0 +1,14 @@
+var onloadCallback = function() {
+    alert("grecaptcha is ready!");
+  };
+function checkRecaptcha() {
+    var response = grecaptcha.getResponse();
+    if(response.length == 0) { 
+        //reCaptcha not verified
+        alert("no pass"); 
+      }
+      else { 
+        //reCaptch verified
+        alert("pass"); 
+      }
+  }
